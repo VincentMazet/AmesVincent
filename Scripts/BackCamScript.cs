ec9b6a3fe84e38e274e8ffa83fe0621106ecef9a
YannPenven <YannPenven@users.noreply.github.com>
2017-01-13 09:19:09 +0100

    add test for brigthness

--- Scripts/BackCamScript.cs
+++ Scripts/BackCamScript.cs
@@ -27,5 +27,20 @@ public class BackCamScript : MonoBehaviour {
 		screen.texture = cam;
 		cam.Play ();
 	}
+	
+	public bool isBrigthnessLow()
+	{
+		var Brigthnesslow = false;
+		var screenWidth = Screen.width;
+		var screenHeigth = Screen.height;
+		var pixel1 = this.cameraBack.GetPixel ((int)System.Math.Ceiling(screenWidth*0.25),(int)System.Math.Ceiling(screenHeigth*0.25));
+		var pixel2 = this.cameraBack.GetPixel ((int)System.Math.Ceiling(screenWidth*0.75),(int)System.Math.Ceiling(screenHeigth*0.75));
+		var brigthness1 = (0.2126 * pixel1.r + 0.7152 * pixel1.g + 0.0722 * pixel1.b);
+		var brigthness2 = (0.2126 * pixel2.r + 0.7152 * pixel2.g + 0.0722 * pixel2.b);
+		if (brigthness1 < (0.2) && brigthness2 < (0.2)) {
+			Brigthnesslow = true;
+		}
+		return Brigthnesslow;
+	}
 		
 }
